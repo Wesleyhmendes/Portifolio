d9e2f5f7acb1a34f55ffee3afb6d8a3470db444d
Wesley Mendes <wesleymendes123321@gmail.com>
2024-04-16 18:23:18 -0300

    Add. Byte for Bite

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,147 +1,108 @@
-import iphoneRecipeApp from '../../assets/projects/iphone_app_receitas.png';
+import iphoneRecipeApp from '../../assets/projects/iphone-byte-for-bite.png';
 import iphoneCodeNews from '../../assets/projects/iphone_site_noticias.png';
 import TFC2 from '../../assets/projects/TFC2.png';
 import wallet from '../../assets/projects/wallet.png';
-import {
-  ProjectH1,
-  ProjectH2,
-  ProjectH4,
-  ProjectImg,
-  ProjectImgDesktop,
-  ProjectP,
-  ProjectSection,
-  ProjectDescriptionDiv,
-  ProjectTagDiv,
-  ProjectTagsReact,
-  ProjectTagsTs,
-  ProjectTagsNode,
-  ProjectTagsFront,
-  ProjectTagsBack,
-  ProjectTagsDocker,
-  ProjectDiv,
-  ProjectLink,
-  ProjectAccessBtn,
-} from './Projects.styles';
+import * as S from './Projects.styles';
 
 function Projects() {
   return (
-    <ProjectSection>
-      <ProjectH1>Principais projetos</ProjectH1>
-      <ProjectDiv>
-        <ProjectImg src={ iphoneRecipeApp } alt="iphone rodando o aplicativo " />
-        <ProjectDescriptionDiv>
-          <ProjectH2>App de receitas</ProjectH2>
-          <ProjectTagDiv>
-            <ProjectH4>2023</ProjectH4>
-            <ProjectTagsFront>Front-end</ProjectTagsFront>
-            <ProjectTagsReact>React</ProjectTagsReact>
-            <ProjectTagsTs>TypeScript</ProjectTagsTs>
-          </ProjectTagDiv>
-          <ProjectP>
-            Um app de receitas com funcionalidade de login, pesquisa de receitas
-            e bebidas, favoritar receita, acompanhar o progresso das receitas
-            e perfil com dados do usuário.
-          </ProjectP>
-          <ProjectLink href="https://github.com/Wesleyhmendes/RecipesApp" target="blanket">
-            <ProjectAccessBtn>
+    <S.ProjectSection>
+      <S.ProjectH1>Principais projetos</S.ProjectH1>
+      <S.ProjectDiv>
+        <S.ProjectImg src={ iphoneRecipeApp } alt="iphone rodando o aplicativo " />
+        <S.ProjectDescriptionDiv>
+          <S.ProjectH2>Byte for Bite</S.ProjectH2>
+          <S.ProjectTagDiv>
+            <S.ProjectH4>2023</S.ProjectH4>
+            <S.ProjectTagsFront>Front-end</S.ProjectTagsFront>
+            <S.ProjectTagsReact>React</S.ProjectTagsReact>
+            <S.ProjectTagsTs>TypeScript</S.ProjectTagsTs>
+            <S.ProjectTagsNode>Node.js</S.ProjectTagsNode>
+          </S.ProjectTagDiv>
+          <S.ProjectP>
+            Meu projeto mais completo. Um aplicativo de receitas full stack
+            com autenticação de login, cadastro, pesquisa de receitas e
+            drinks e tela de perfil.
+            Nele o usuário pode favoritar receitas, deixá-las em progresso,
+            filtrar por categorias e muito mais.
+          </S.ProjectP>
+          <S.ProjectLink href="https://github.com/Wesleyhmendes/Byte-for-Bite" target="blanket">
+            <S.ProjectAccessBtn>
               Acessar projeto
-            </ProjectAccessBtn>
-          </ProjectLink>
-        </ProjectDescriptionDiv>
-      </ProjectDiv>
+            </S.ProjectAccessBtn>
+          </S.ProjectLink>
+        </S.ProjectDescriptionDiv>
+      </S.ProjectDiv>
 
-      <ProjectDiv>
-        <ProjectDescriptionDiv>
-          <ProjectH2>Football Table</ProjectH2>
-          <ProjectTagDiv>
-            <ProjectH4>2023</ProjectH4>
-            <ProjectTagsBack>Back-end</ProjectTagsBack>
-            <ProjectTagsNode>Node.js</ProjectTagsNode>
-            <ProjectTagsTs>TypeScript</ProjectTagsTs>
-          </ProjectTagDiv>
-          <ProjectP>
+      <S.ProjectDiv>
+        <S.ProjectDescriptionDiv>
+          <S.ProjectH2>Site de notícias</S.ProjectH2>
+          <S.ProjectTagDiv>
+            <S.ProjectH4>2023</S.ProjectH4>
+            <S.ProjectTagsFront>Front-end</S.ProjectTagsFront>
+            <S.ProjectTagsReact>React</S.ProjectTagsReact>
+            <S.ProjectTagsTs>TypeScript</S.ProjectTagsTs>
+          </S.ProjectTagDiv>
+          <S.ProjectP>
+            Uma página que mostra de forma atualizada as últimas notícias
+            do site do IBGE. Nesta aplicação podemos ver cards de notícias,
+            favoritar e filtrar por data, tipo ou favoritas.
+          </S.ProjectP>
+          <S.ProjectLink href="https://github.com/Wesleyhmendes/CodeNews" target="blanket">
+            <S.ProjectAccessBtn>
+              Acessar projeto
+            </S.ProjectAccessBtn>
+          </S.ProjectLink>
+        </S.ProjectDescriptionDiv>
+        <S.ProjectImg src={ iphoneCodeNews } alt="iphone rodando o aplicativo " />
+      </S.ProjectDiv>
+
+      <S.ProjectDiv>
+        <S.ProjectDescriptionDiv>
+          <S.ProjectH2>Football Table</S.ProjectH2>
+          <S.ProjectTagDiv>
+            <S.ProjectH4>2023</S.ProjectH4>
+            <S.ProjectTagsBack>Back-end</S.ProjectTagsBack>
+            <S.ProjectTagsNode>Node.js</S.ProjectTagsNode>
+            <S.ProjectTagsTs>TypeScript</S.ProjectTagsTs>
+          </S.ProjectTagDiv>
+          <S.ProjectP>
             Uma Api com endpoints que mostram a tabela
             em ordem de pontos, vitórias e saldo de gols. Também é possível
             criar partidas, filtrar resultados e fazer login com validação
             de token, email e senha.
-          </ProjectP>
-          <ProjectLink href="https://github.com/Wesleyhmendes/Football-Table" target="blanket">
-            <ProjectAccessBtn>
+          </S.ProjectP>
+          <S.ProjectLink href="https://github.com/Wesleyhmendes/Football-Table" target="blanket">
+            <S.ProjectAccessBtn>
               Acessar projeto
-            </ProjectAccessBtn>
-          </ProjectLink>
-        </ProjectDescriptionDiv>
-        <ProjectImgDesktop src={ TFC2 } alt="notebook rodando o aplicativo " />
-      </ProjectDiv>
+            </S.ProjectAccessBtn>
+          </S.ProjectLink>
+        </S.ProjectDescriptionDiv>
+        <S.ProjectImgDesktop src={ TFC2 } alt="notebook rodando o aplicativo " />
+      </S.ProjectDiv>
 
-      <ProjectDiv>
-        <ProjectImg src={ iphoneCodeNews } alt="iphone rodando o aplicativo " />
-        <ProjectDescriptionDiv>
-          <ProjectH2>Site de notícias</ProjectH2>
-          <ProjectTagDiv>
-            <ProjectH4>2023</ProjectH4>
-            <ProjectTagsFront>Front-end</ProjectTagsFront>
-            <ProjectTagsReact>React</ProjectTagsReact>
-            <ProjectTagsTs>TypeScript</ProjectTagsTs>
-          </ProjectTagDiv>
-          <ProjectP>
-            Uma página que mostra de forma atualizada as últimas notícias
-            do site do IBGE. Nesta aplicação podemos ver cards de notícias,
-            favoritar e filtrar por data, tipo ou favoritas.
-          </ProjectP>
-          <ProjectLink href="https://github.com/Wesleyhmendes/CodeNews" target="blanket">
-            <ProjectAccessBtn>
-              Acessar projeto
-            </ProjectAccessBtn>
-          </ProjectLink>
-        </ProjectDescriptionDiv>
-      </ProjectDiv>
-
-      <ProjectDiv>
-        <ProjectDescriptionDiv>
-          <ProjectH2>Blogs Api</ProjectH2>
-          <ProjectTagDiv>
-            <ProjectH4>2023</ProjectH4>
-            <ProjectTagsBack>Back-end</ProjectTagsBack>
-            <ProjectTagsNode>Node.js</ProjectTagsNode>
-            <ProjectTagsDocker>Docker</ProjectTagsDocker>
-          </ProjectTagDiv>
-          <ProjectP>
+      <S.ProjectDiv>
+        <S.ProjectDescriptionDiv>
+          <S.ProjectH2>Blogs Api</S.ProjectH2>
+          <S.ProjectTagDiv>
+            <S.ProjectH4>2023</S.ProjectH4>
+            <S.ProjectTagsBack>Back-end</S.ProjectTagsBack>
+            <S.ProjectTagsNode>Node.js</S.ProjectTagsNode>
+            <S.ProjectTagsDocker>Docker</S.ProjectTagsDocker>
+          </S.ProjectTagDiv>
+          <S.ProjectP>
             Uma Api com princípios do REST e um banco de dados para produção
             de conteúdo para um blog usando Node.js com Sequelize. É possível fazer login
             com token, além de criar, deletar e editar um post.
-          </ProjectP>
-          <ProjectLink href="https://github.com/Wesleyhmendes/blogs-api-back-end" target="blanket">
-            <ProjectAccessBtn>
+          </S.ProjectP>
+          <S.ProjectLink href="https://github.com/Wesleyhmendes/blogs-api-back-end" target="blanket">
+            <S.ProjectAccessBtn>
               Acessar projeto
-            </ProjectAccessBtn>
-          </ProjectLink>
-        </ProjectDescriptionDiv>
-      </ProjectDiv>
-
-      <ProjectDiv>
-        <ProjectDescriptionDiv>
-          <ProjectH2>Wallet App</ProjectH2>
-          <ProjectTagDiv>
-            <ProjectH4>2023</ProjectH4>
-            <ProjectTagsFront>Front-end</ProjectTagsFront>
-            <ProjectTagsReact>React</ProjectTagsReact>
-            <ProjectTagsTs>Redux</ProjectTagsTs>
-          </ProjectTagDiv>
-          <ProjectP>
-            Uma aplicação que faz o controle de gastas com conversor de moedas. É
-            possível adicionar, remover ou editar um gasto, além de visualizar a
-            tabela de gastos e o total de gastos convertidos para qualquer moeda.
-          </ProjectP>
-          <ProjectLink href="https://github.com/Wesleyhmendes/Wallet" target="blanket">
-            <ProjectAccessBtn>
-              Acessar projeto
-            </ProjectAccessBtn>
-          </ProjectLink>
-        </ProjectDescriptionDiv>
-        <ProjectImgDesktop src={ wallet } alt="notebook rodando o aplicativo " />
-      </ProjectDiv>
-    </ProjectSection>
+            </S.ProjectAccessBtn>
+          </S.ProjectLink>
+        </S.ProjectDescriptionDiv>
+      </S.ProjectDiv>
+    </S.ProjectSection>
   );
 }
 
