58e8a339a9243a668d2742552ad5177fb00ff0f8
Wesley Mendes <wesleymendes123321@gmail.com>
2024-05-30 14:59:28 -0300

    Fix stylezation of projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable max-len */
+/* eslint-disable react/jsx-max-depth */
 import iphoneRecipeApp from '../../assets/projects/iphone-byte-for-bite.png';
 import iphoneCodeNews from '../../assets/projects/iphone_site_noticias.png';
 import TFC2 from '../../assets/projects/TFC2.png';
@@ -6,100 +8,114 @@ import * as S from './Projects.styles';
 function Projects() {
   return (
     <S.ProjectSection>
-      <S.ProjectH1>Principais projetos</S.ProjectH1>
+      <h1>Principais projetos</h1>
       <S.ProjectDiv>
-        <S.ProjectImg src={ iphoneRecipeApp } alt="iphone rodando o aplicativo " />
-        <S.ProjectDescriptionDiv>
-          <S.ProjectH2>Byte for Bite</S.ProjectH2>
-          <S.ProjectTagDiv>
-            <S.ProjectH4>2023</S.ProjectH4>
-            <S.ProjectTagsFront>Front-end</S.ProjectTagsFront>
-            <S.ProjectTagsReact>React</S.ProjectTagsReact>
-            <S.ProjectTagsTs>TypeScript</S.ProjectTagsTs>
-            <S.ProjectTagsNode>Node.js</S.ProjectTagsNode>
-          </S.ProjectTagDiv>
-          <S.ProjectP>
+        <div className="img-div">
+          <img className="iphone-pic" src={ iphoneRecipeApp } alt="iphone rodando o aplicativo " />
+        </div>
+        <div>
+          <h2>Byte for Bite</h2>
+          <div>
+            <h4>2023</h4>
+            <div className="tags-div">
+              <button className="front-end-tag">Front-end</button>
+              <button className="react-tag">React</button>
+              <button className="ts-tag">TypeScript</button>
+              <button className="node-tag">Node.js</button>
+            </div>
+          </div>
+          <p>
             Meu projeto mais completo. Um aplicativo de receitas full stack
             com autenticação de login, cadastro, pesquisa de receitas e
             drinks e tela de perfil.
             Nele o usuário pode favoritar receitas, deixá-las em progresso,
             filtrar por categorias e muito mais.
-          </S.ProjectP>
-          <S.ProjectLink href="https://github.com/Wesleyhmendes/Byte-for-Bite" target="blanket">
-            <S.ProjectAccessBtn>
+          </p>
+          <a href="https://github.com/Wesleyhmendes/Byte-for-Bite" target="blanket">
+            <button className="acessar-projeto">
               Acessar projeto
-            </S.ProjectAccessBtn>
-          </S.ProjectLink>
-        </S.ProjectDescriptionDiv>
+            </button>
+          </a>
+        </div>
       </S.ProjectDiv>
 
       <S.ProjectDiv>
-        <S.ProjectDescriptionDiv>
-          <S.ProjectH2>Site de notícias</S.ProjectH2>
-          <S.ProjectTagDiv>
-            <S.ProjectH4>2023</S.ProjectH4>
-            <S.ProjectTagsFront>Front-end</S.ProjectTagsFront>
-            <S.ProjectTagsReact>React</S.ProjectTagsReact>
-            <S.ProjectTagsTs>TypeScript</S.ProjectTagsTs>
-          </S.ProjectTagDiv>
-          <S.ProjectP>
+        <div className="img-div">
+          <img className="iphone-pic" src={ iphoneCodeNews } alt="iphone rodando o aplicativo " />
+        </div>
+        <div>
+          <h2>Site de notícias</h2>
+          <div>
+            <h4>2023</h4>
+            <div className="tags-div">
+              <button className="front-end-tag">Front-end</button>
+              <button className="react-tag">React</button>
+              <button className="ts-tag">TypeScript</button>
+            </div>
+          </div>
+          <p>
             Uma página que mostra de forma atualizada as últimas notícias
             do site do IBGE. Nesta aplicação podemos ver cards de notícias,
             favoritar e filtrar por data, tipo ou favoritas.
-          </S.ProjectP>
-          <S.ProjectLink href="https://github.com/Wesleyhmendes/CodeNews" target="blanket">
-            <S.ProjectAccessBtn>
+          </p>
+          <a href="https://github.com/Wesleyhmendes/CodeNews" target="blanket">
+            <button className="acessar-projeto">
               Acessar projeto
-            </S.ProjectAccessBtn>
-          </S.ProjectLink>
-        </S.ProjectDescriptionDiv>
-        <S.ProjectImg src={ iphoneCodeNews } alt="iphone rodando o aplicativo " />
+            </button>
+          </a>
+        </div>
       </S.ProjectDiv>
 
       <S.ProjectDiv>
-        <S.ProjectDescriptionDiv>
-          <S.ProjectH2>Football Table</S.ProjectH2>
-          <S.ProjectTagDiv>
-            <S.ProjectH4>2023</S.ProjectH4>
-            <S.ProjectTagsBack>Back-end</S.ProjectTagsBack>
-            <S.ProjectTagsNode>Node.js</S.ProjectTagsNode>
-            <S.ProjectTagsTs>TypeScript</S.ProjectTagsTs>
-          </S.ProjectTagDiv>
-          <S.ProjectP>
+        <div className="img-div">
+          <img className="mac-pic" src={ TFC2 } alt="notebook rodando o aplicativo " />
+        </div>
+        <div>
+          <h2>Football Table</h2>
+          <div>
+            <h4>2023</h4>
+            <div className="tags-div">
+              <button className="backend-tag">Back-end</button>
+              <button className="node-tag">Node.js</button>
+              <button className="ts-tag">TypeScript</button>
+            </div>
+          </div>
+          <p>
             Uma Api com endpoints que mostram a tabela
             em ordem de pontos, vitórias e saldo de gols. Também é possível
             criar partidas, filtrar resultados e fazer login com validação
             de token, email e senha.
-          </S.ProjectP>
-          <S.ProjectLink href="https://github.com/Wesleyhmendes/Football-Table" target="blanket">
-            <S.ProjectAccessBtn>
+          </p>
+          <a href="https://github.com/Wesleyhmendes/Football-Table" target="blanket">
+            <button className="acessar-projeto">
               Acessar projeto
-            </S.ProjectAccessBtn>
-          </S.ProjectLink>
-        </S.ProjectDescriptionDiv>
-        <S.ProjectImgDesktop src={ TFC2 } alt="notebook rodando o aplicativo " />
+            </button>
+          </a>
+        </div>
       </S.ProjectDiv>
 
       <S.ProjectDiv>
-        <S.ProjectDescriptionDiv>
-          <S.ProjectH2>Blogs Api</S.ProjectH2>
-          <S.ProjectTagDiv>
-            <S.ProjectH4>2023</S.ProjectH4>
-            <S.ProjectTagsBack>Back-end</S.ProjectTagsBack>
-            <S.ProjectTagsNode>Node.js</S.ProjectTagsNode>
-            <S.ProjectTagsDocker>Docker</S.ProjectTagsDocker>
-          </S.ProjectTagDiv>
-          <S.ProjectP>
+        <div>
+          <h2>Blogs Api</h2>
+          <div>
+            <h4>2023</h4>
+            <div className="tags-div">
+              <button className="backend-tag">Back-end</button>
+              <button className="node-tag">Node.js</button>
+              <button className="docker-tag">Docker</button>
+            </div>
+          </div>
+          <p>
             Uma Api com princípios do REST e um banco de dados para produção
             de conteúdo para um blog usando Node.js com Sequelize. É possível fazer login
             com token, além de criar, deletar e editar um post.
-          </S.ProjectP>
-          <S.ProjectLink href="https://github.com/Wesleyhmendes/blogs-api-back-end" target="blanket">
-            <S.ProjectAccessBtn>
+          </p>
+          <a href="https://github.com/Wesleyhmendes/blogs-api-back-end" target="blanket">
+            <button className="acessar-projeto">
               Acessar projeto
-            </S.ProjectAccessBtn>
-          </S.ProjectLink>
-        </S.ProjectDescriptionDiv>
+            </button>
+          </a>
+        </div>
       </S.ProjectDiv>
     </S.ProjectSection>
   );
